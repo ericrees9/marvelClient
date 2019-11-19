6231e96b203e1e84fa7c13aef13a6e40de207ead
erees <erees@protonmail.com>
2019-11-19 16:04:01 -0500

    Building out API functionality.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import ComicCard from './components/ComicCard/ComicCard.js';
+
+const baseURL = "https://gateway.marvel.com:443/v1/public/"
+const APIKey = ""
+
+// console.log(APIKey);
+// console.log(baseURL + APIKey);
+
+const Excelsior = (title) => {
+  // e.preventDefault();
+  let url = `${baseURL}comics?title=${title}&apikey=${APIKey}`;
+  console.log(title)
+
+  fetch(url)
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.error({ message: err }))
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Marvel Comic Book Search</h1>
+      <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="exampleSearch" className="mr-sm-2">Your Comic</Label>
+          <Input type="searc" name="search" id="exampleSearch" />
+        </FormGroup>
+        <Button 
+          onClick={(event) => Excelsior(event.target.value)}
+        >Excelsior!</Button>
+      </Form>
+      <ComicCard></ComicCard>
     </div>
   );
 }
---
+++ src/components/ComicCard/ComicCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ComicCard.module.css';
+
+const ComicCard = () => (
+  <div className={styles.ComicCard}>
+    ComicCard Component
+  </div>
+);
+
+ComicCard.propTypes = {};
+
+ComicCard.defaultProps = {};
+
+export default ComicCard;
