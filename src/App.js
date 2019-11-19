01c2773a82a1f18e956a5ac5679942fdb8b18d19
erees <erees@protonmail.com>
2019-11-19 16:29:07 -0500

    Fixing the search function.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import ComicCard from './components/ComicCard/ComicCard.js';
@@ -9,28 +9,30 @@ const APIKey = "6ac68c640e567a0be876ac9a65ba411f"
 // console.log(APIKey);
 // console.log(baseURL + APIKey);
 
-const Excelsior = (title) => {
-  // e.preventDefault();
-  let url = `${baseURL}comics?title=${title}&apikey=${APIKey}`;
-  console.log(title)
-
-  fetch(url)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error({ message: err }))
-}
-
 function App() {
+  const [search, setSearch] = useState("");
+  
+  const Excelsior = (e) => {
+    e.preventDefault();
+    let url = `${baseURL}comics?title=${search}&apikey=${APIKey}`;
+    console.log(search)
+  
+    fetch(url)
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.error({ message: err }))
+  }
+
   return (
     <div className="App">
       <h1>Marvel Comic Book Search</h1>
       <Form inline>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="exampleSearch" className="mr-sm-2">Your Comic</Label>
-          <Input type="searc" name="search" id="exampleSearch" />
+          <Input type="searc" name="search" id="exampleSearch" onChange={(e) => setSearch(e.target.value)} />
         </FormGroup>
         <Button 
-          onClick={(event) => Excelsior(event.target.value)}
+          onClick={(e) => Excelsior(e)}
         >Excelsior!</Button>
       </Form>
       <ComicCard></ComicCard>
