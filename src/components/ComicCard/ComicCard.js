ddf5ec3ed895c55ed84d2a22e76e231b43d957e8
erees <erees@protonmail.com>
2019-11-19 23:05:18 -0500

    Adding static picture for now.

--- src/components/ComicCard/ComicCard.js
+++ src/components/ComicCard/ComicCard.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import styles from './ComicCard.module.css';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
+/* src={`${props.comicData.images[0].path}.jpg`} */
+
 const ComicCard = (props) => (
   <div className={styles.ComicCard}>
     <Card>
-        <CardImg top width="100%" src={`${props.comicData.images.path}.jpg`} alt="Card image cap" />
+    <CardImg top width="100%" src="http://i.annihil.us/u/prod/marvel/i/mg/a/03/53430272978fa.jpg" alt="Card image cap" />
         <CardBody>
           <CardTitle>{props.comicData.title}</CardTitle>
-          <CardSubtitle>{props.comicData.series.name}</CardSubtitle>
+          <CardSubtitle>{props.comicData.description}</CardSubtitle>
           <Button>Read More</Button>
         </CardBody>
       </Card>
