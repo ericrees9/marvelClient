79d4edd33747a39db1c5978909ff237f4e42d4c6
erees <erees@protonmail.com>
2019-11-20 15:25:46 -0500

    Added individual comic fetch to ComicPage.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import TestData from '../src/testData.js';
 
 const baseURL = "https://gateway.marvel.com:443/v1/public/"
 const APIKey = "6ac68c640e567a0be876ac9a65ba411f"
-let COMIC_ROUTE = "/comicpage/:comicId/"
+let COMIC_ROUTE = "/comicpage/:comicId"
 
 // console.log(APIKey);
 // console.log(baseURL + APIKey);
--- src/components/ComicPage/ComicPage.js
+++ src/components/ComicPage/ComicPage.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ComicPage.module.css';
 
-const ComicPage = (props) => (
-  <div className={styles.ComicPage}>
-    Hello this is the Comic Page!
-  </div>
-);
+const ComicPage = (props) => {
+  const [ singleComic, setSingleComic ] = useState([]);
+
+  useLayoutEffect(() => {
+    // console.log('useEffect')
+    fetchComic();
+  }, [])
+  
+  const fetchComic = () => {
+    let uuid = (window.location.pathname).slice(11)
+    console.log(uuid);
+    let url = "https://gateway.marvel.com:443/v1/public/comics/" + uuid + "?apikey=6ac68c640e567a0be876ac9a65ba411f"
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {setSingleComic(res); console.log(res)})
+      .catch(err => console.error({ message: err })) 
+}
+
+  return(
+    <div className={styles.ComicPage}>
+      Hello this is the Comic Page!
+    </div>
+  );
+
+};
 
 ComicPage.propTypes = {};
 
