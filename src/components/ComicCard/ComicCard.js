fbda4ee9556a87cc00868bfc9901597438ac7c4a
erees <erees@protonmail.com>
2019-11-19 22:15:06 -0500

    Building out the ComicCard component with more data.

--- src/components/ComicCard/ComicCard.js
+++ src/components/ComicCard/ComicCard.js
@@ -6,12 +6,11 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } fr
 const ComicCard = (props) => (
   <div className={styles.ComicCard}>
     <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardImg top width="100%" src={props.comicData.images[0].path} alt="Card image cap" />
         <CardBody>
           <CardTitle>{props.comicData.title}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
+          <CardSubtitle>{props.comicData.series.name}</CardSubtitle>
+          <Button>Read On</Button>
         </CardBody>
       </Card>
   </div>
