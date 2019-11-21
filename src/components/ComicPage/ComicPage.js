5cb9a88aaf28a7da89d9518aafb3497292ddced1
erees <erees@protonmail.com>
2019-11-21 10:30:33 -0500

    Think I fixed the ComicPage not rendering.

--- src/components/ComicPage/ComicPage.js
+++ src/components/ComicPage/ComicPage.js
@@ -28,55 +28,55 @@ const ComicPage = (props) => {
 
   return(
     <div className="main">
-      {/* <h1 className="comicTitle">{singleComic.data.results[0].title}</h1>
+      <h1 className="comicTitle">{singleComic.title}</h1>
       <div className="resultsArea">
         <div className="left">
-          <img className="img" src={`${singleComic.data.results[0].images[0].path}.jpg`} />
+          <img className="img" src={`${singleComic.images[0].path}.jpg`} />
         </div>
         <div className="right">
           <ListGroup className="listArea">
             <ListGroupItem>
               <ListGroupItemHeading>Created By</ListGroupItemHeading>
                 <ListGroupItemText>
-                {singleComic.data.results[0].creators.items[0].name}
+                {singleComic.creators.items[0].name}
                 </ListGroupItemText>
             </ListGroupItem>
             <ListGroupItem>
               <ListGroupItemHeading>Description</ListGroupItemHeading>
                 <ListGroupItemText>
-                {singleComic.data.results[0].description}
+                {singleComic.description}
                 </ListGroupItemText>
             </ListGroupItem>
             <ListGroupItem>
               <ListGroupItemHeading>On Sale Date</ListGroupItemHeading>
                 <ListGroupItemText>
-                {moment(singleComic.data.results[0].dates[0].date).format("MMMM Do YYYY")}
+                {moment(singleComic.dates[0].date).format("MMMM Do YYYY")}
                 </ListGroupItemText>
             </ListGroupItem>
             <ListGroupItem>
               <ListGroupItemHeading>Original Print Price</ListGroupItemHeading>
                 <ListGroupItemText>
-                ${singleComic.data.results[0].prices[0].price}
+                ${singleComic.prices[0].price}
                 </ListGroupItemText>
             </ListGroupItem>
             <ListGroupItem>
               <ListGroupItemHeading>Pages</ListGroupItemHeading>
                 <ListGroupItemText>
-                {singleComic.data.results[0].pageCount}
+                {singleComic.pageCount}
                 </ListGroupItemText>
             </ListGroupItem>
             <ListGroupItem>
-              <ListGroupItemHeading><a target="_blank" href={`${singleComic.data.results[0].urls[0].url}`}>More Details</a></ListGroupItemHeading>
+              <ListGroupItemHeading><a target="_blank" href={`${singleComic.urls[0].url}`}>More Details</a></ListGroupItemHeading>
             </ListGroupItem>
             <ListGroupItem>
-              <ListGroupItemHeading><a target="_blank" href={`${singleComic.data.results[0].urls[1].url}`}>Buy Now</a></ListGroupItemHeading>
+              <ListGroupItemHeading><a target="_blank" href={`${singleComic.urls[1].url}`}>Buy Now</a></ListGroupItemHeading>
             </ListGroupItem>
             <ListGroupItem>
               <ListGroupItemHeading><Link to="/">Back to Search</Link></ListGroupItemHeading>
             </ListGroupItem>
           </ListGroup>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 
